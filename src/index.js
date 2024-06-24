905da95f80b89edf35a376ded5912d9061476d07
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-24 17:18:37 -0400

    add component to the root element

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import DisplayWord from './DisplayWord';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DisplayWord/>
+    <App/>
   </React.StrictMode>
 );
 
