dae9dc69f39e05593b57dd555783deb300394e6e
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-24 17:12:46 -0400

    add react state in the parrent component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import React,{useReducer, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
+  const [angryApp, dispatch] = useReducer((angryApp, amount) => {
+   return angryApp > 1 ? 0 : angryApp + amount;
+  },0);
+
+  
+  const [light, setLight] = useState('off');
+  const darkMode = (light === "off")?'dark':"";
+  const switchLight= () => {
+    setLight(light === 'on'?'off':'on');
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${darkMode}`}  >
+      <h1>{angryApp < 1 ? "Fancy Buttons!" : "YOU'RE CLICKING TOO MANY BUTTONS!"}</h1>
+      <section>
+        <AngryButton increaseAnger={dispatch}/>
+        <CounterButton increaseAnger={dispatch}/>
+        <LightSwitchButton light={light}  switchLight={switchLight} increaseAnger={dispatch}/>
+        <TextRepeaterButton increaseAnger={dispatch}/>
+      </section>
+    
     </div>
   );
 }
